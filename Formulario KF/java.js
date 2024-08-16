bc933e75215ce2ac2bf8a1da4bbbb59328b49ebc
Thekf7 <omar.kv1013@gmail.com>
2024-08-16 09:57:54 -0600

    .

---
+++ Formulario KF/java.js
@@ -0,0 +1,14 @@
+var Kevin = "nombre";
+var Fernandez = "apellido";
+var Siuu = "apodo";
+var edad = "edad";
+
+
+function login (){
+    let nombre= document.getElementById("nombre").value; 
+    let apellido= document.getElementById("apellido").value; 
+    let apodo= document.getElementById("apodo").value; 
+    let edad= document.getElementById("edad").value; 
+ 
+
+}
